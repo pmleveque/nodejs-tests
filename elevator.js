dff1b8608acd01cf662169ff837bfce2d17ad591
pmleveque <pmleveque@users.noreply.github.com>
2015-03-31 22:58:32 -0700

    elevator game
    http://play.elevatorsaga.com

--- elevator.js
+++ elevator.js
@@ -1,59 +1,91 @@
+// play it on http://play.elevatorsaga.com
 {
     init: function(elevators, floors) {
 
-        var elevator = elevators[0];
         var topFloor = floors.length - 1;
-        var lastFloor = -1;
 
-        elevator.displayGoUp = function () {
-            this.goingUpIndicator(true);
-            this.goingDownIndicator(false);
-        }
+        var floorsUpState = [];
+        var floorsDownState = [];
+        // fix it:
+        var elevatorsState = [];
 
-        elevator.displayGoDown = function () {
-            this.goingUpIndicator(false);
-            this.goingDownIndicator(true);
-        }
+        elevators.forEach(function (elevator) {
+            elevator.callsUp = [];
+            elevator.callsDown = [];
+            elevator.direction = "";
 
-        elevator.goAndUpdate = function (floorNum) {
-            this.goToFloor(floorNum);
-            if (this.currentFloor() <= floorNum && floorNum != lastFloor) {
-                this.displayGoUp();
-            } else {
-                this.displayGoDown();
+            elevator.displayGoAll = function () {
+                this.goingUpIndicator(true);
+                this.goingDownIndicator(true);
             }
-        }
 
-        // first
-        elevator.displayGoUp();
-
-        elevator.on("floor_button_pressed", function(floorNum) {
-            elevator.goAndUpdate(floorNum);
-        });
-
-        elevator.on("idle", function() {
-            this.goAndUpdate(0);
-        });
-
-        elevator.on("passing_floor", function(floorNum, direction) {
-            if (direction == "up") {
-                this.displayGoUp();
-            } else {
-                this.displayGoDown();
+            elevator.displayGoUp = function () {
+                this.goingUpIndicator(true);
+                this.goingDownIndicator(false);
             }
+
+            elevator.displayGoDown = function () {
+                this.goingUpIndicator(false);
+                this.goingDownIndicator(true);
+            }
+
+            elevator.goAndUpdate = function (floorNum) {
+                this.goToFloor(floorNum);
+                if (this.currentFloor() <= floorNum) {
+                    this.displayGoUp();
+                } else {
+                    this.displayGoDown();
+                }
+            }
+
+
+            // first
+            elevator.displayGoUp();
+
+            elevator.on("floor_button_pressed", function(floorNum) {
+                elevator.goToFloor(floorNum);
+                //affiner: up or down
+                this.callsUp[floorNum] = 0;
+                this.callsDown[floorNum] = 0;
+            });
+
+            elevator.on("idle", function() {
+                this.goAndUpdate(0);
+            });
+
+            elevator.on("stopped_at_floor", function(floorNum) {
+                // affiner: go up or down?
+                floorsUpState[floorNum] = 0;
+                this.callsUp[floorNum] = 0;
+                this.callsDown[floorNum] = 0;
+
+                if (floorNum == topFloor) {
+                    this.displayGoDown();
+                };
+            });
+
+            elevator.on("passing_floor", function(floorNum, direction) {
+                this.direction = direction;
+            });
         });
 
-        elevator.on("stopped_at_floor", function(floorNum) {
-            if (floorNum == topFloor) {
-                this.displayGoDown();
-            };
+
+
+        floors.forEach(function (floor) {
+            floor.on("up_button_pressed", function() {
+                floorsUpState[this.floorNum()] += 1;
+                elevators[1].goToFloor(this.floorNum());
+            });
+
+            floor.on("down_button_pressed", function() {
+                floorsDownState[this.floorNum()] += 1;
+                elevators[2].goToFloor(this.floorNum());
+            });
         });
-
-
 
 
     },
     update: function(dt, elevators, floors) {
-    // We normally don't need to do anything here
+        console.log
     }
 }
